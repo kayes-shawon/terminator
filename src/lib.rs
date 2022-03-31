c1f17be977b4ca13c84674bba4c9256d29f07918
kayes <foridur.kayes@ucb.com.bd>
2022-03-31 13:37:28 +0600

    test added

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,5 @@
 use std::fs;
-
 use std::error::Error;
-
-pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -26,4 +16,40 @@ impl Config {
 
         Ok(Config { query, filename })
     }
+}
+
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    // println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "dict";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
 }
